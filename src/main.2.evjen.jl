db44ecbb2c69765a21b9e1ea8953fd8a8c55257e
Daniel Steinmaurer <dan.steinmaurer@gmail.com>
2025-03-29 20:20:39 +0100

    add evjen approach scripts

---
+++ src/main.2.evjen.jl
@@ -0,0 +1,31 @@
+using Logging
+Logging.global_logger(ConsoleLogger(Logging.Debug))
+
+include(joinpath(
+                 dirname(@__FILE__),
+                 "funcs.jl"
+                ))
+using .funcs: calculatesum_evjen, normalizer
+
+@debug ARGS
+n0 = if length(ARGS) >= 1
+    parse(Int, ARGS[1])
+else 3 end
+
+@assert n0 >= 3
+V0 = calculatesum_evjen(n0-2)
+V0 
+println("V₀=\t$(V0)")
+V1 = calculatesum_evjen(n0)
+println("V₁=\t$(V1)")
+
+Δ = abs(V1-V0)
+dV = Δ/normalizer(V1)
+println("Δ =\t$(Δ)")
+println("dV =\t$(dV)")
+
+if dV < 5e-4
+    println("solution is excat")
+else
+    println("solution is to inexcat")
+end
---
+++ src/main.evjen.jl
@@ -0,0 +1,11 @@
+using Logging
+Logging.global_logger(ConsoleLogger(Logging.Debug))
+
+include(joinpath(
+                 dirname(@__FILE__),
+                 "funcs.jl"
+                ))
+using .funcs: calculatesum_evjen, normalizer, iterate
+
+start_value = 3
+println("convergent value $(iterate(start_value; func=calculatesum_evjen))")
